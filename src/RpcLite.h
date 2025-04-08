4f0750b8c6ae02d5e0aae91464457969caa7604f
lucio <lucio.rossi75@gmail.com>
2025-04-08 18:31:13 +0200

    mod: rearranged main header

---
+++ src/RpcLite.h
@@ -0,0 +1,17 @@
+//
+// Created by lucio on 4/8/25.
+//
+
+#ifndef RPCLITE_H
+#define RPCLITE_H
+
+#include "Arduino.h"
+
+#include "rpc.h"
+#include "transport.h"
+#include "client.h"
+#include "server.h"
+
+#include "DummyTransport.h"
+
+#endif //RPCLITE_H
