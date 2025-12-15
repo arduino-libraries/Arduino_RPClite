2a4f8646a6ddd5f92d58330603aabdd7bea4e19a
Luca Burelli <l.burelli@arduino.cc>
2025-12-15 12:42:48 +0100

    MsgPack: set default log level to WARN to avoid CI issues
    Not setting the default log level causes a warning on every CI run.
    Setting it to WARN avoids the warning and is a reasonable default.
    
    Signed-off-by: Luca Burelli <l.burelli@arduino.cc>

--- src/decoder.h
+++ src/decoder.h
@@ -6,11 +6,13 @@
     This Source Code Form is subject to the terms of the Mozilla Public
     License, v. 2.0. If a copy of the MPL was not distributed with this
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
-    
 */
 
 #ifndef RPCLITE_DECODER_H
 #define RPCLITE_DECODER_H
+
+// MsgPack log level
+#define DEBUGLOG_DEFAULT_LOG_LEVEL_WARN
 
 #include "MsgPack.h"
 #include "transport.h"
